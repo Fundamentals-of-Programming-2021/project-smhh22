39868f0a026796aac739e92fbf020e21d8df1684
smhh22 <s.m.m.m.h2013@gmail.com>
2022-01-17 17:06:52 +0330

    add files that remained untracked in last commit

---
+++ src/read_map.h
@@ -0,0 +1,61 @@
+//bemellah
+#include "header.h"
+
+void Read_map(char *str) {
+	FILE* Map_file = fopen(str, "r");
+
+	fscanf(Map_file, "%d ", &NUMBER_OF_PLAYERS);
+	fscanf(Map_file, "%d ", &GRID_WIDTH);
+	fscanf(Map_file, "%d ", &GRID_HEIGHT);
+
+	Players = (PLAYER*)malloc(sizeof(PLAYER) * NUMBER_OF_PLAYERS);
+	GRID = (CELL**)malloc(sizeof(CELL*) * GRID_WIDTH);
+	CASTLE_PTRS = (CASTLE***)malloc(sizeof(CASTLE**) * GRID_WIDTH);
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		GRID[i] = (CELL*)malloc(sizeof(CELL) * GRID_HEIGHT);
+		CASTLE_PTRS[i] = (CASTLE**)malloc(sizeof(CASTLE*) * GRID_HEIGHT);
+	}
+
+	for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
+		fscanf(Map_file, "%x ", &Players[i].Color);
+		Players[i].Potion_enabled = -1;
+		Players[i].Soldiers_count = 0;
+	}
+
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			for (int k = 0; k < 4; k++) {
+				fscanf(Map_file, "%d ", &GRID[i][j].Border_shown[k]);
+			}
+
+			int x, y;
+			fscanf(Map_file, "%d %d ", &x, &y);
+			if (CASTLE_PTRS[x][y] != NULL) {
+				GRID[i][j].Castle_ptr = CASTLE_PTRS[x][y];
+			}
+			else {
+				GRID[i][j].Castle_ptr = CASTLE_PTRS[x][y] = (CASTLE*)malloc(sizeof(CASTLE));
+			}
+		}
+	}
+
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			char* mode = (char*)malloc(sizeof(char) * 30);
+			fscanf(Map_file, "%s ", mode);
+
+			if (mode[0] != 'N') {
+				fscanf(Map_file, "%d ", &(CASTLE_PTRS[i][j]->Soldiers_count));
+				int p;
+				fscanf(Map_file, "%d ", &p);
+				CASTLE_PTRS[i][j]->Player = Players + p;
+				CASTLE_PTRS[i][j]->Player->Soldiers_count += CASTLE_PTRS[i][j]->Soldiers_count;
+
+				TOTAL_SOLDIERS_COUNT += CASTLE_PTRS[i][j]->Soldiers_count;
+			}
+
+			free(mode);
+		}
+	}
+	fclose(Map_file);
+}
---
+++ src/show.h
@@ -0,0 +1,51 @@
+//besmellah
+#include "read_map.h"
+
+void show_map(SDL_Renderer*);
+void show_bar(SDL_Renderer*);
+
+void show_map(SDL_Renderer *renderer) {
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			boxColor(renderer, GRID[i][j].x1, GRID[i][j].y1, GRID[i][j].x2, GRID[i][j].y2, GRID[i][j].Castle_ptr->Player->Color | (GRID[i][j].Castle_ptr->Soldiers_count * 0x01000000));
+
+			if (GRID[i][j].Border_shown[0]) {
+				thickLineColor(renderer, GRID[i][j].x1, GRID[i][j].y1, GRID[i][j].x2, GRID[i][j].y1, BORDER_THICKNESS, 0xff000000);
+			}
+
+			if (GRID[i][j].Border_shown[1]) {
+				thickLineColor(renderer, GRID[i][j].x2, GRID[i][j].y1, GRID[i][j].x2, GRID[i][j].y2, BORDER_THICKNESS, 0xff000000);
+			}
+
+			if (GRID[i][j].Border_shown[2]) {
+				thickLineColor(renderer, GRID[i][j].x1, GRID[i][j].y2, GRID[i][j].x2, GRID[i][j].y2, BORDER_THICKNESS, 0xff000000);
+			}
+
+			if (GRID[i][j].Border_shown[3]) {
+				thickLineColor(renderer, GRID[i][j].x1, GRID[i][j].y1, GRID[i][j].x1, GRID[i][j].y2, BORDER_THICKNESS, 0xff000000);
+			}
+			
+			if (CASTLE_PTRS[i][j] != NULL) {
+				int mx = (GRID[i][j].x1 + GRID[i][j].x2) / 2;
+				int my = (GRID[i][j].y1 + GRID[i][j].y2) / 2;
+				boxColor(renderer, mx - CASTLE_SIZE / 2, my - CASTLE_SIZE / 2, mx + CASTLE_SIZE / 2, my + CASTLE_SIZE / 2, GRID[i][j].Castle_ptr->Player->Color | 0xff000000);
+
+//				stringColor(renderer, mx - CASTLE_SIZE / 2, my - CASTLE_SIZE / 2, "SALAM:( ", GRID[i][j].Castle_ptr->Player->Color ^ 0xffffffff);
+			}
+		}
+	}
+}
+
+void show_bar(SDL_Renderer *renderer) {
+	if (!TOTAL_SOLDIERS_COUNT) {
+		printf("Error loading bar: Division by zero not allowed.\n");
+		fflush(stdout);
+	}
+
+	double sum = 0;
+	for (int i = 1; i < NUMBER_OF_PLAYERS; i++) {
+		thickLineColor(renderer, WINDOW_PADDING_LEFT + (int)(sum / TOTAL_SOLDIERS_COUNT * WIDTH), WINDOW_PADDING_UP / 2, WINDOW_PADDING_LEFT + (int)((sum + Players[i].Soldiers_count) / TOTAL_SOLDIERS_COUNT * WIDTH), WINDOW_PADDING_UP / 2, BAR_THICKNESS, Players[i].Color | 0xff000000);
+		sum += Players[i].Soldiers_count;
+	}
+	thickLineColor(renderer, WINDOW_PADDING_LEFT + (int)(sum / TOTAL_SOLDIERS_COUNT * WIDTH), WINDOW_PADDING_UP / 2, WINDOW_PADDING_LEFT + (int)((sum + Players[0].Soldiers_count) / TOTAL_SOLDIERS_COUNT * WIDTH), WINDOW_PADDING_UP / 2, BAR_THICKNESS, Players[0].Color | 0xff000000);
+}
